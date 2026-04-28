443b69c6dc163b3f6d5835c9963dcc10b2f69442
Marcell Neszt <sylwolveryn@gmail.com>
2026-04-28 21:38:23 +0200

    update colors a bit

--- vike-sandbox/pages/+Layout.tsx
+++ vike-sandbox/pages/+Layout.tsx
@@ -21,20 +21,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                         navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
                         padding="md"
                 >
-                    <AppShell.Header>
-                        <Group h="100%" px="md">
-                            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm"/>
-                            <a href="/">
-                                {' '}
-                                <Image h={50} fit="contain" src={logoUrl}/>{' '}
-                            </a>
-                        </Group>
-                    </AppShell.Header>
-                    <AppShell.Navbar p="md">
-                        <Link href="/" label="D3"/>
-                        <Link href="/todo" label="Todo"/>
-                        <Link href="/star-wars" label="Data Fetching"/>
-                    </AppShell.Navbar>
                     <AppShell.Main> {children} </AppShell.Main>
                 </AppShell>
             </MantineProvider>
